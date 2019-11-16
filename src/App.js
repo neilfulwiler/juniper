e58950801ce4f64519bc2d3782e079b1b37866af
Neil Fulwiler <neil.fulwiler@gmail.com>
2019-11-16 02:48:02 +0000

    built basic checklist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import 'typeface-roboto';
+import IconButton from '@material-ui/core/IconButton';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import classNames from 'classnames';
+
+const todos = [
+  {name: 'link to shard transitions from i/titan'},
+  {name: 'initialize traces in mqtt'},
+  {name: 'remove delta payload fields from queue entry'},
+  {name: 'roll out one iris'},
+];
+
+function Todo({name}) {
+  const [value, setValue] = useState(name);
+  const [editing, setEditing] = useState(false);
+  const [checked, setChecked] = useState(false);
+  const content = editing ? (
+      <input 
+        className="todoInput"
+        value={value} 
+        onChange={e => setValue(e.target.value)}
+      />
+     ) : <div className={classNames({completedTask: checked})} onClick={() => setEditing(true)}>{value}</div>;
+
+  return (
+    <div className="todo">
+      <IconButton onClick={() => setChecked(!checked)}>
+        {checked ? <CheckBoxIcon/> : <CheckBoxOutlineBlankIcon/>}
+      </IconButton>
+      {content}
+    </div>
+  );
+}
 
 function App() {
+  const [value, setValue] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="list">
+        {todos.map(({name}, idx) => (
+          <Todo key={idx} name={name}/>
+        ))}
+      </div>
     </div>
   );
 }
