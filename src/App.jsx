2d37dede587e8623bfc02866cf387787d2735474
Neil Fulwiler <neil.fulwiler@gmail.com>
2019-11-18 00:09:49 +0000

    add header with noop login/signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,36 @@ import React from 'react';
 import Todos from './Todos';
 
 
+function logIn() {}
+function signUp() {
+
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <div
+        className="header-button"
+        onClick={() => logIn()}
+      >
+        Log In
+      </div>
+      <div>|</div>
+      <div
+        className="header-button"
+        onClick={() => signUp()}
+      >
+        Sign Up
+      </div>
+    </div>
+  );
+}
+
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <Todos />
     </div>
   );
