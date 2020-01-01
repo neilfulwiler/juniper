f545d678dba20d2f0eb9abfe0eecf00005e41552
Neil Fulwiler <neil.fulwiler@gmail.com>
2020-01-01 21:14:10 +0000

    add fake app list

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import firebase from '@firebase/app';
 import '@firebase/auth';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
+import Popper from '@material-ui/core/Popper';
+import { makeStyles } from '@material-ui/core/styles';
 import AppsIcon from '@material-ui/icons/Apps';
+import Typography from '@material-ui/core/Typography';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { logIn, logOut } from '../redux/actions/user';
@@ -13,9 +19,62 @@ function signUp() {
 
 }
 
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+const AppList: React.FC<{anchorEl: HTMLElement}> = ({ anchorEl }: {anchorEl: HTMLElement}) => {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Popper open anchorEl={anchorEl}>
+      <Card>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+              Word of the Day
+          </Typography>
+          <Typography variant="h5" component="h2">
+              be
+            {bull}
+  nev
+            {bull}
+  o
+            {bull}
+  lent
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+              adjective
+          </Typography>
+          <Typography variant="body2" component="p">
+              well meaning and kindly.
+            <br />
+              "a benevolent smile"
+          </Typography>
+        </CardContent>
+      </Card>
+    </Popper>
+  );
+};
+
 export default function Header() {
   const dispatch = useDispatch();
   const user = useSelector<State, User | undefined>((state) => state.user.entity);
+  const [appListButtonEl, setAppListButtonEl] = useState<HTMLElement | null>(null);
+  const [showAppsList, setShowAppsList] = useState(false);
 
   useEffect(() => {
     firebase.auth && firebase.auth().onAuthStateChanged((currentUser: any) => {
@@ -39,10 +98,11 @@ export default function Header() {
         Sign Out
         </div>
         <div className="header-button">
-          <IconButton>
+          <IconButton ref={(el) => appListButtonEl !== el && setAppListButtonEl(el)} onClick={() => setShowAppsList(!showAppsList)}>
             <AppsIcon />
           </IconButton>
         </div>
+        {showAppsList && appListButtonEl && <AppList anchorEl={appListButtonEl} />}
       </>
     )
     : (
